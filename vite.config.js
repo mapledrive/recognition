f9a6e4a96b3ee7f2c64ca49de34d915c6eef620d
mapledrive <sait.sharifullin@gmail.com>
2024-07-10 18:28:54 +0500

    second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/recognition/',
   plugins: [react()],
-})
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/process': 'https://realmonte.pythonanywhere.com/process', // Ensure the full path is included
+      '/test': 'https://realmonte.pythonanywhere.com/test',
+    },
+  },
+});
