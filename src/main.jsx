5015e495a5a4bce737e3b67136581cab5d2d0181
mapledrive <sait.sharifullin@gmail.com>
2024-07-10 18:41:05 +0500

    third

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useRef, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { SketchField } from 'react-sketch';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const Layout = () => {
+  const sketchRef = useRef(null);
+  const [error, setError] = useState('');
+  const [prediction, setPrediction] = useState('');
+
+  const handleSubmit = async () => {
+    const image = sketchRef.current.toDataURL();
+
+    const response = await fetch(
+      'https://realmonte.pythonanywhere.com/process',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image }),
+      }
+    );
+
+    const text = await response.text();
+
+    setPrediction(text);
+
+    setError(undefined);
+  };
+
+  const handleClear = e => {
+    sketchRef.current.clear();
+    setPrediction('');
+  };
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Draw a digit here:</p>
+        <div className='holder'>
+          <div className='blob-1 child1'></div>
+          <div className='sketchWrapper child2'>
+            <SketchField
+              ref={sketchRef}
+              width='100%'
+              height='100%'
+              lineColor='#111'
+              lineWidth={10}
+            />
+          </div>
+        </div>
+
+        <p>Predicted value is: {prediction || ' '}</p>
+        <div className='controls'>
+          <button onClick={handleClear}>Clear</button>
+          <button onClick={handleSubmit}>Guess the number</button>
+        </div>
+        {error && <p className='message'>Something went wrong</p>}
+      </header>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Layout />);
+
+// Добавить более современную библиотеку для работы с canvas
+// https://www.npmjs.com/package/react-canvas-draw
+// https://www.npmjs.com/package/react-konva
+
+// Описание проекта: фронт отправляет изображение в Python при помощи Data URL
+// Бэк на Python создает из Data URL обьект изображения Python Image Library
+// Несколько функций преобразовывают обьект изображения в формат подходящий для модели
+// машинного обучения.
