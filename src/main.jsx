b45e116531a597dd7ffea9f4361d8508c0efe3ea
mapledrive <sait.sharifullin@gmail.com>
2024-07-17 00:12:11 +0500

    environment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { SketchField } from 'react-sketch';
 import './index.css';
 
+// Function to retrieve base URL based on environment
+export const getBaseUrl = () => {
+  if (import.meta.env.MODE === 'production') {
+    console.log('We are in production environment now ', import.meta.env.BASE_URL);
+    return 'https://realmonte.pythonanywhere.com';
+  } else {
+    console.log('We are in development environment now ', import.meta.env.BASE_URL);
+    return 'http://127.0.0.1:5000';
+  }
+};
+
 const Layout = () => {
   const sketchRef = useRef(null);
   const [error, setError] = useState('');
@@ -11,16 +22,15 @@ const Layout = () => {
   const handleSubmit = async () => {
     const image = sketchRef.current.toDataURL();
 
-    const response = await fetch(
-      'https://realmonte.pythonanywhere.com/process',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ image }),
-      }
-    );
+    const baseUrl = getBaseUrl();
+
+    const response = await fetch(`${baseUrl}/process`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ image }),
+    });
 
     const text = await response.text();
 
@@ -34,28 +44,22 @@ const Layout = () => {
     setPrediction('');
   };
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>Draw a digit here:</p>
-        <div className='holder'>
-          <div className='blob-1 child1'></div>
-          <div className='sketchWrapper child2'>
-            <SketchField
-              ref={sketchRef}
-              width='100%'
-              height='100%'
-              lineColor='#111'
-              lineWidth={10}
-            />
+        <div className="holder">
+          <div className="blob-1 child1"></div>
+          <div className="sketchWrapper child2">
+            <SketchField ref={sketchRef} width="100%" height="100%" lineColor="#111" lineWidth={10} />
           </div>
         </div>
 
         <p>Predicted value is: {prediction || ' '}</p>
-        <div className='controls'>
+        <div className="controls">
           <button onClick={handleClear}>Clear</button>
           <button onClick={handleSubmit}>Guess the number</button>
         </div>
-        {error && <p className='message'>Something went wrong</p>}
+        {error && <p className="message">Something went wrong</p>}
       </header>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/recognition/',
+  base: '/',
   plugins: [react()],
   server: {
     open: true,
     port: 3000,
-    proxy: {
-      '/process': 'https://realmonte.pythonanywhere.com/process',
-    },
   },
 });
